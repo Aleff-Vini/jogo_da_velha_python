48ee66d9ee21349edae7ad409604091ae8a1e9d2
Aleff-Vini <aleffvn123@gmail.com>
2024-10-10 19:56:36 -0300

    arquivo jogo da velha em pygame

---
+++ jogo_da_velha.py
@@ -0,0 +1,32 @@
+# Example file showing a basic pygame "game loop"
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+cor_sel=0
+cores =["blue","red","green","yellow","white","brown"]
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            cor_sel += 1
+            print(cor_sel)
+    if cor_sel==len(cores):
+     cor_sel=0
+ 
+    screen.fill(cores[cor_sel])
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
