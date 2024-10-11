e0ea75fa1d5dd88ce1693a18eafc0b3f1600f774
Aleff-Vini <aleffvn123@gmail.com>
2024-10-10 22:34:03 -0300

    aproximação do codigo para o jogo da velha

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,29 +1,35 @@
-# Example file showing a basic pygame "game loop"
+#Importa a biblioteca pygame
 import pygame
 
-# pygame setup
-pygame.init()
-pygame.display.set_caption("Jogo da Velha")
-icon = pygame.image.load("icone.png")  
-pygame.display.set_icon(icon)  
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-cor_sel=0
-cores =["blue","red","green","yellow","white","brown"]
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+# Configuração pygame
+pygame.init() #inicialização do pygame
+pygame.display.set_caption("Jogo da Velha") #nome da janela
+icon = pygame.image.load("icone.png")  #define qual é o logo 
+pygame.font.init() #Inicializa o pacote de fontes do pygame
+pygame.display.set_icon(icon)   #define o logo no janela ao lado do titulo 
+screen = pygame.display.set_mode((1280, 720)) #define o tamanho da janela
+clock = pygame.time.Clock() #biblioteca de tempo
+running = True #variavel que indica que está "funcionado/aberto"
+font = pygame.font.SysFont("Impact", 74)  # 'None' usa a fonte padrão, 74 é o tamanho da fonte
+person_X = font.render("X",True,"red")
+person_O = font.render("O",True,"blue")
+click=0
+while running:      #loop enquanto a variavel running for true 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            cor_sel += 1
-            print(cor_sel)
-    if cor_sel==len(cores):
-     cor_sel=0
- 
-    screen.fill(cores[cor_sel])
+        if event.type == pygame.QUIT: #evento que registra o click no "X" da janela
+            running = False           #quando for clicado define a variavel running como false e encerra o jogo
+        if event.type==pygame.MOUSEBUTTONDOWN:
+             click += 1
+             if(click>2):
+                click=1
+    # Limpa a tela
+    screen.fill("black")
+
+    # Desenha X ou O com base no contador de cliques
+    if click == 1:
+        screen.blit(person_X, (500, 350))  # Desenha "X"
+    elif click == 2:
+        screen.blit(person_O, (500, 350))  # Desenha "O"
 
     # RENDER YOUR GAME HERE
 
